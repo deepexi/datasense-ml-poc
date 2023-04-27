83b70280dc059fe38b32ba5d7e32d373578467d8
dormi330 <dormi330@gmail.com>
2023-04-27 16:12:37 +0800

    refactor: test to e2e

--- src/test/java/com/deepexi/ds/ast/visitor/e2e/SqlValidateTest.java
+++ src/test/java/com/deepexi/ds/ast/visitor/e2e/SqlValidateTest.java
@@ -1,9 +1,10 @@
-package com.deepexi.ds.ast.visitor;
+package com.deepexi.ds.ast.visitor.e2e;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import com.deepexi.ds.ast.AstNode;
+import com.deepexi.ds.ast.visitor.JdbcUtils;
 import com.deepexi.ds.ast.visitor.generator.SqlGenerator;
 import com.deepexi.ds.ast.visitor.generator.SqlGeneratorContext;
 import com.deepexi.ds.ast.visitor.generator.SqlGeneratorPgContext;
@@ -15,7 +16,7 @@ import org.junit.jupiter.api.Test;
 
 /**
  * <pre>
- * 将生成的sql 与手写的sql进行比较, 检测有消息
+ * 将生成的sql 与手写的sql进行比较, 检测有效性
  * 在这里进行 测试的 yml, 必须有 YmlDebug节点, 手写sql, 比对结果
  * </pre>
  */
@@ -25,7 +26,7 @@ public class SqlValidateTest {
   public void testVisitMetricBindQuery_case04() {
     YmlFullQuery ctx = YmlFullQueryParser.loadFromRes("tpcds/02_biz/case04.yml");
     YmlDebug ymlDebug = ctx.getYmlDebug();
-    assertNotNull(ymlDebug, "这里测试的yml, 必须有YmlDebug节点");
+    assertNotNull(ymlDebug, "e2e测试, 必须有YmlDebug节点");
     String manualSql = ymlDebug.getSql();
 
     // generate sql
